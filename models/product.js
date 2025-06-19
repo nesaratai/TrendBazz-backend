9ed36ef9c9a3f590b8281e4de11578a72fe060b5
nesaratai <nesaratai@gmail.com>
2025-06-18 21:19:05 -0400

    ...

--- models/product.js
+++ models/product.js
@@ -12,10 +12,12 @@ const productSchema = new mongoose.Schema({
     price: {
         type: Number,
         required: true,
+        min: 0,
     },
     stock: {
         type: Number,
         default: 0,
+        min: 0,
     },
     img: {
         type: String,
